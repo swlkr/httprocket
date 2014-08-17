45f0ca6946828a10635b568be3442ca35266f5bb
Sean Walker <swalker@mdstaff.com>
2014-08-17 14:56:04 -0700

    Add a description for the nuget package

--- HttpRocket/Properties/AssemblyInfo.cs
+++ HttpRocket/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HttpRocket")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple, testable http library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sean Walker")]
 [assembly: AssemblyProduct("HttpRocket")]
