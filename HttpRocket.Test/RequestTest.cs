dbe58cbbd00cc6fe70ce57e5d409a77d161834cf
Sean Walker <swalker@mdstaff.com>
2014-09-23 11:48:05 -0700

    Add headers. Unify formatting.

---
+++ HttpRocket.Test/RequestTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HttpRocket;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HttpRocket.Test {
+	[TestClass]
+	public class RequestTest {
+
+		[TestMethod]
+		public void CreateRequestWithAuthorizationHeader() {
+			var r = new Request("http://httpstat.us/200");
+			r.CreateRequest("GET", "", new System.Collections.Generic.Dictionary<string, string>() { { "Authorization", "OAuth xxxxx" } });
+			Assert.AreEqual("OAuth xxxxx", r.HttpWebRequest.Headers.Get("Authorization"));
+		}
+	}
+}
--- HttpRocket/HttpRocket.cs
+++ HttpRocket/HttpRocket.cs
@@ -1,79 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
 
-namespace HttpRocket
-{
-    public interface IHttpRocket
-    {
-        Response Get();
+namespace HttpRocket {
+	public interface IHttpRocket {
+		Response Get();
 		Response Put(string requestData);
 		Response Post(string requestData);
 		Response Delete();
-        string ContentType { get; set; }
-        string Method { get; set; }
-    }
+		string ContentType { get; set; }
+		string Method { get; set; }
+	}
 
-    public class Request : IHttpRocket
-    {
-        private readonly HttpWebRequest httpWebRequest;
-        public string ContentType { get; set; }
-        public string Method
-        {
-            get { return httpWebRequest.Method; }
-            set { httpWebRequest.Method = value; }
-        }
+	public class Request : IHttpRocket {
+		public HttpWebRequest HttpWebRequest { get; set; }
+		public string ContentType { get; set; }
+		public string Url { get; set; }
+		public string Method { get; set; }
 
-        public Request(string url)
-            : this((HttpWebRequest)WebRequest.Create(url)) { }
+		public Request(string url) {
+			Url = url;
+			ContentType = ContentType ?? "application/x-www-form-urlencoded";
+		}
 
-        public Request(HttpWebRequest httpWebRequest)
-        {
-            this.httpWebRequest = httpWebRequest;
-            ContentType = "application/x-www-form-urlencoded";
-        }
+		public void CreateRequest(string method, string requestData, Dictionary<string, string> headers) {
+			HttpWebRequest = (HttpWebRequest)WebRequest.Create(Url);
+			HttpWebRequest.Method = method;
+			HttpWebRequest.ContentType = ContentType;
+			HttpWebRequest.ContentLength = 0;
+			foreach (var header in headers) {
+				HttpWebRequest.Headers.Add(header.Key, header.Value);
+			}
 
-        public Response Get()
-        {
-            return MakeRequest(WebRequestMethods.Http.Get, null);
-        }
+			SetRequestData(requestData);
+		}
 
-		public Response Post(string requestData)
-        {
-            return MakeRequest(WebRequestMethods.Http.Post, requestData);
-        }
+		public Response Get() {
+			return MakeRequest(WebRequestMethods.Http.Get, null);
+		}
 
-		public Response Put(string requestData)
-        {
-            return MakeRequest(WebRequestMethods.Http.Put, requestData);
-        }
+		public Response Post(string requestData) {
+			return MakeRequest(WebRequestMethods.Http.Post, requestData);
+		}
+
+		public Response Put(string requestData) {
+			return MakeRequest(WebRequestMethods.Http.Put, requestData);
+		}
 
 		public Response Delete() {
 			return MakeRequest("DELETE", null);
 		}
 
-		private Response MakeRequest(string method, string requestData)
-        {
-            httpWebRequest.Method = method;
-            httpWebRequest.ContentType = ContentType;
-            httpWebRequest.ContentLength = 0;
+		public Response MakeRequest(string method, string requestData) {
+			return MakeRequest(method, requestData, new Dictionary<string, string>());
+		}
 
-            try
-            {
-                SetRequestData(requestData);
-				
-                using( var response = httpWebRequest.GetResponse())
-                using( var responseStream = response.GetResponseStream())
-                using (var responseStreamReader = new StreamReader(responseStream)){
-	                
+		public Response MakeRequest(string method, string requestData, Dictionary<string, string> headers) {
+			CreateRequest(method, requestData, headers);
+
+			try {
+				using (var response = HttpWebRequest.GetResponse())
+				using (var responseStream = response.GetResponseStream())
+				using (var responseStreamReader = new StreamReader(responseStream)) {
+
 					var responseFromServer = responseStreamReader.ReadToEnd();
 					return new Response(((HttpWebResponse)response).StatusCode, responseFromServer);
 
-                }
-            }
-            catch (System.Net.WebException e)
-            {
+				}
+			} catch (System.Net.WebException e) {
 				using (var response = e.Response) {
 					using (var responseStream = response.GetResponseStream()) {
 						using (var responseStreamReader = new StreamReader(responseStream)) {
@@ -82,41 +78,36 @@ namespace HttpRocket
 						}
 					}
 				}
-            }
-        }
+			}
+		}
 
-        private void SetRequestData(string requestData)
-        {
-            if (String.IsNullOrEmpty(requestData)) return;
+		public void SetRequestData(string requestData) {
+			if (String.IsNullOrEmpty(requestData)) return;
 
-            var requestDataBuffer = Encoding.Default.GetBytes(requestData);
-            httpWebRequest.ContentLength = requestDataBuffer.Length;
-            httpWebRequest.GetRequestStream()
-                .Write(requestDataBuffer, 0, requestDataBuffer.Length);
-        }
-    }
+			var requestDataBuffer = Encoding.Default.GetBytes(requestData);
+			HttpWebRequest.ContentLength = requestDataBuffer.Length;
+			HttpWebRequest.GetRequestStream()
+				.Write(requestDataBuffer, 0, requestDataBuffer.Length);
+		}
+	}
 
-    public class Response
-    {
-        private readonly HttpStatusCode httpStatusCode;
-        private readonly string responseBody;
+	public class Response {
+		private readonly HttpStatusCode httpStatusCode;
+		private readonly string responseBody;
 
-        public Response() { }
+		public Response() { }
 
-        public Response(HttpStatusCode httpStatusCode, string responseBody)
-        {
-            this.httpStatusCode = httpStatusCode;
-            this.responseBody = responseBody;
-        }
+		public Response(HttpStatusCode httpStatusCode, string responseBody) {
+			this.httpStatusCode = httpStatusCode;
+			this.responseBody = responseBody;
+		}
 
-        public virtual string ResponseBody
-        {
-            get { return responseBody; }
-        }
+		public virtual string ResponseBody {
+			get { return responseBody; }
+		}
 
-        public virtual HttpStatusCode StatusCode
-        {
-            get { return httpStatusCode; }
-        }
-    }
+		public virtual HttpStatusCode StatusCode {
+			get { return httpStatusCode; }
+		}
+	}
 }
